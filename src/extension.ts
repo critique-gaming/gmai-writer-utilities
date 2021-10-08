b0d57eec46ac3f547f8869ac0e84aff932a677ed
Marius Petcu <marius@petcu.me>
2021-10-08 16:45:27 +0300

    Implement SSML speech

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,146 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { config as awsConfig, Polly, SharedIniFileCredentials } from 'aws-sdk';
+import * as tmp from 'tmp';
+import { exec } from 'child_process';
+import { Readable } from 'stream';
+import { createWriteStream } from 'fs';
+import { writeFile } from 'fs/promises';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gmai-writer-utilities" is now active!');
+const interpolationsToBreak = (s: string) =>
+  s.replace(
+    /\${[a-zA-Z_][a-zA-Z_0-9]*}/s,
+    '<prosody volume="silent">something</prosody>'
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gmai-writer-utilities.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GMAI writer utilities!');
+const richtextTags: Set<string> = new Set([
+  "a",
+  "b",
+  "br",
+  "shadow",
+  "outline",
+  "font",
+  "i",
+  "img",
+  "nobr",
+  "size",
+  "spine",
+  // "p", // This is valid in both
+  "repeat",
+]);
+
+const stripTags = (s: string, tags: Set<string>): string =>
+	s.replace(/<\/?([a-zA-Z_][a-zA-Z0-9:_\-]*)[^>]*>/g, (match, capture1) => 
+		tags.has(capture1) ? "" : match
+	);
+
+async function playVoice(selection: string) {
+	selection = selection.replace(/^\s*[a-z_][a-z0-9_]+\s*(\[[a-z0-9_]+\]\s*)/, '');
+
+	if (!selection.includes('<speak>')) {
+			selection = `<speak>${selection}</speak>`;
+	}
+
+	selection = stripTags(interpolationsToBreak(selection), richtextTags);
+
+	if (selection === "") { return; }
+	console.log(`Saying: ${selection}`);
+
+	let conf = vscode.workspace.getConfiguration('gmai-writer-utilities');
+
+	awsConfig.credentials = new SharedIniFileCredentials({
+			profile: conf.awsProfile
+	});
+	const polly = new Polly({
+			region: conf.awsRegion
 	});
 
+	let audioStream: Buffer | Readable;
+	try {
+		/* eslint-disable @typescript-eslint/naming-convention */
+		const stream = (await polly.synthesizeSpeech({
+			OutputFormat: 'mp3',
+			Text: selection,
+			TextType: 'ssml',
+			VoiceId: conf.pollyVoice,
+			Engine: conf.pollyEngine,
+			LanguageCode: conf.pollyLanguage,
+		}).promise()).AudioStream;
+		/* eslint-enable @typescript-eslint/naming-convention */
+
+		if (stream === undefined) {
+			throw(new Error("Empty response"));
+		}
+		audioStream = (stream as any);
+	} catch (err) {
+		vscode.window.showErrorMessage(`Speech synthesis error: ${err}`);
+		console.error(err);
+		return;
+	}
+
+	const { path } = await new Promise((resolve, reject) => {
+		tmp.file({
+			prefix: 'gmai-speech-', 
+			postfix: '.mp3', 
+			discardDescriptor: true,
+		}, (err: Error | null, path: string, fd: number, cleanup: Function) => {
+			if (err) { reject(err); }
+			resolve({ path });
+		});
+	});
+
+	if (audioStream instanceof Readable) {
+		const audioStreamStream: Readable = audioStream;
+		await new Promise((resolve, reject) => {
+			let toStream = createWriteStream(path);
+			toStream.on('finish', resolve);
+			toStream.on('error', reject);
+			audioStreamStream.pipe(toStream);
+		});
+	} else if (audioStream instanceof Buffer) {
+		await writeFile(path, audioStream);
+	} else {
+		throw new Error("AWS returned a weird object");
+	}
+
+	let cmd = '';
+	switch (process.platform) {
+			case 'darwin': {
+					cmd = `osascript -e 'tell application "QuickTime Player"' -e 'set theMovie to open POSIX file "${path}"' -e 'tell theMovie to play' -e 'end tell'`;
+					break;
+			}
+			case 'win32': {
+					cmd = `start ${path}`;
+					break;
+			}
+			default: {
+					cmd = `xdg-open ${path}`;
+					break;
+			}
+	}
+
+	await new Promise<void>((resolve, reject) => {
+		exec(cmd, {}, (err: Error | null, stdout: string, stderr: string) => {
+			if (err) { reject(err); }
+			resolve();
+		});
+	});
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('gmai-writer-utilities.speakSSML', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor === undefined) { return; }
+
+		const selection = editor.selection.isEmpty
+			? editor.document.lineAt(editor.selection.start.line).text
+			: editor.document.getText(editor.selection);
+
+		playVoice(selection).catch(err => {
+			vscode.window.showErrorMessage(err.toString());
+			console.error(err);
+		});
+	});
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
